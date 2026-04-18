8145081828258cf75069de2e51395061f20c2171
pppopipupu <pppopipupu@gmail.com>
2026-04-18 23:23:13 +0800

    fix js deploy x2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: "/pppopipupu_blog",
+  assetPrefix: "/pppopipupu_blog",
 };
 
 export default nextConfig;
