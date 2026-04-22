f9776e053c7a7cc776270df4ebfcaf13affc51e7
pppopipupu <pppopipupu@gmail.com>
2026-04-22 23:24:04 +0800

    cloudflare fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  supabaseUrl || "https://tmp.supabase.co", 
+  supabaseAnonKey || "tmp"
+);
+
+
